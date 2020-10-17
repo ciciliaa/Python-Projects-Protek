b4d588f32744a106713a97e2e645980560f6f14b
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-10-18 01:23:06 +0700

    Create chapter4coba4.py

---
+++ chapter4coba4.py/chapter4coba4.py
@@ -0,0 +1,9 @@
+# script hitung waktu sampai
+
+jarak1 = 125
+kecepatan1 = 62
+jarak2 = 256
+kecepatan2 = 70
+jamBerangkat = 6
+menitIstirahat = - 45
+print((jarak1 / kecepatan1) + (jarak2 / kecepatan2) / 60)
