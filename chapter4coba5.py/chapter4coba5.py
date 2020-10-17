4d09a49e8921213cc9625684aa5f0ffe16bc73bb
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-10-18 01:23:10 +0700

    Create chapter4coba5.py

---
+++ chapter4coba5.py/chapter4coba5.py
@@ -0,0 +1,6 @@
+# script membuat diagram jumlah mahasiswa
+
+jumlahMahasiswaLakiLaki = 100
+jumlahMahasiswaPerempuan = 150
+print(str('*' * (jumlahMahasiswaLakiLaki)))
+print(str('*' * (jumlahMahasiswaPerempuan)))
