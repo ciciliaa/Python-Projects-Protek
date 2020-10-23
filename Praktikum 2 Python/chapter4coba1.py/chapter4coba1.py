0c8232dc3455e0470290bd9b10d43ab376f1cf82
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-10-23 14:21:03 +0700

    Create chapter4coba1.py

---
+++ Praktikum 2 Python/chapter4coba1.py/chapter4coba1.py
@@ -0,0 +1,9 @@
+# script hitung tarif rental
+
+tarif1 = 200000
+tarif2 = 10000
+jamMulai = 6
+menitMulai = - 0
+jamSelesai = 23
+menitSelesai = - 50
+print((jamSelesai - jamMulai) + (menitSelesai - menitMulai) / 60)
