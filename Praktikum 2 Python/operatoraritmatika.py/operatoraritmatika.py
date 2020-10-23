5ebb643ab55db952b90d7e98466cdc92f9439c8e
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-10-23 14:21:28 +0700

    Create operatoraritmatika.py

---
+++ Praktikum 2 Python/operatoraritmatika.py/operatoraritmatika.py
@@ -0,0 +1,37 @@
+x = 10
+y = 20.2
+
+print(type(x+x))
+print(type(y+x))
+print(type(x+y))
+print(type(y+y))
+
+print(type(x-x))
+print(type(y-x))
+print(type(x-y))
+print(type(y-y))
+
+print(type(x*x))
+print(type(y*x))
+print(type(x*y))
+print(type(y*y))
+
+print(type(x/x))
+print(type(y/x))
+print(type(x/y))
+print(type(y/y))
+
+print(type(x//x))
+print(type(y//x))
+print(type(x//y))
+print(type(y//y))
+
+print(type(x%x))
+print(type(y%x))
+print(type(x%y))
+print(type(y%y))
+
+print(type(x**x))
+print(type(y**x))
+print(type(x**y))
+print(type(y**y))
