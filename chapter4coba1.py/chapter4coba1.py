b31ce4f6dfe199504af56cbd293f23c1b1fc63d9
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-10-23 14:19:58 +0700

    Delete chapter4coba1.py

--- chapter4coba1.py/chapter4coba1.py
+++
@@ -1,9 +0,0 @@
-# script hitung tarif rental
-
-tarif1 = 200000
-tarif2 = 10000
-jamMulai = 6
-menitMulai = - 0
-jamSelesai = 23
-menitSelesai = - 50
-print((jamSelesai - jamMulai) + (menitSelesai - menitMulai) / 60)
