f1ea81e4b1d52d443d6bdf6cd074d31cf8afd645
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-10-23 14:20:16 +0700

    Delete chapter4coba4.py

--- chapter4coba4.py/chapter4coba4.py
+++
@@ -1,9 +0,0 @@
-# script hitung waktu sampai
-
-jarak1 = 125
-kecepatan1 = 62
-jarak2 = 256
-kecepatan2 = 70
-jamBerangkat = 6
-menitIstirahat = - 45
-print((jarak1 / kecepatan1) + (jarak2 / kecepatan2) / 60)
