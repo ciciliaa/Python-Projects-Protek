724ee2f29455df8f8d93f2ef38bc66e3ad6ab878
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-11-08 16:35:28 +0700

    Create Praktikum3latihan2.py

---
+++ Praktikum 3 Python/Praktikum3latihan2.py/Praktikum3latihan2.py
@@ -0,0 +1,10 @@
+bhsIndonesia = -1
+matematika = 70
+ipa = 65
+
+if(bhsIndonesia < 60) and (matematika < 60):
+    print("LULUS")
+elif(ipa < 0) and (bhsIndonesia < 0):
+    print("TIDAK LULUS")
+else:
+    print("Maaf input ada yang tidak valid")
