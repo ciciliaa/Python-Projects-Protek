48d69fb44e0893c46e918416ad80ab9c78aa7cac
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-11-08 16:33:45 +0700

    Create chapter06latihan2.py

---
+++ Chapter06/chapter06latihan2/chapter06latihan2.py
@@ -0,0 +1,29 @@
+def starFormation1(n):
+    kolom = 4
+    baris = 4
+
+    i = 0
+    while(i < baris):
+        j = 0
+        while(j < kolom):
+            print('*')
+            j += 1
+        print('')
+        i += 1
+        kolom += 1
+
+
+
+
+    kolom = 4
+    baris = 4
+
+    i = 0
+    while(i <= baris):
+        j = 0
+        while(j < kolom):
+            print('*')
+            j += 1
+        print('')
+        i += 1
+        kolom -= 1
