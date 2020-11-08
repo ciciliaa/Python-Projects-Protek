933f39a427176c84b64428abf014844766b794bb
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-11-08 16:35:32 +0700

    Create Praktikum3latihan3.py

---
+++ Praktikum 3 Python/Praktikum3latihan3.py/Praktikum3latihan3.py
@@ -0,0 +1,10 @@
+bhsIndonesia = -1
+matematika = 70
+ipa = 65
+if(bhsIndonesia < 60) and (matematika < 60):
+    print("LULUS")
+if(bhsIndonesia < 60) or (matematika < 70):
+    print("TIDAK LULUS")
+    print("Sebab :")
+    print("Nilai bahasa indonesia kurang dari 60")
+    print("Nilai matematikanya kurang dari 70")
