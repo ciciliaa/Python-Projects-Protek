e48ff3036033a06f5282711ef5991b923972adf3
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-11-08 16:33:53 +0700

    Create statistik.py

---
+++ Chapter06/chapter06latihan4/statistik.py/statistik.py
@@ -0,0 +1,11 @@
+def sum(a, b, c, d, d, e, f, g, h, i, j, k):
+    return(sum)
+
+def average(a, b, c, d, e, f, g, h, i, j, k):
+    return(average)
+
+def maks(a, b, c, d, e, f, g, h, i, j, k):
+    return(maks)
+
+def min(a, b, c, d, e, f, g, h, i, j, k):
+    return(min)
