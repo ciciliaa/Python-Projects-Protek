bee2be30c84002a55b6b8e5eeca575107eb40641
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-11-08 16:36:12 +0700

    Create Praktikum3Prak2latihan3.py

---
+++ Praktikum 3 Python/Praktikum3Prak2latihan3.py/Praktikum3Prak2latihan3.py
@@ -0,0 +1,10 @@
+hitung = 0
+sum = 0
+for i in range (1, 100, 2):
+    print(i)
+    hitung = hitung + 1
+    sum = sum + i
+    
+
+print('Banyak bilangan ganjil : ' + str(hitung))
+print('Jumlah seluruh bilangan : ' + str(sum))
