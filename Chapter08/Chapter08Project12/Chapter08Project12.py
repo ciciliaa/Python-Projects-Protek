26c57f94c387db8a9545daaaff42a78a6f72103f
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-12-06 21:35:46 +0700

    Create Chapter08Project12.py

---
+++ Chapter08/Chapter08Project12/Chapter08Project12.py
@@ -0,0 +1,30 @@
+buah = {'apel' : 5000,
+        'jeruk' : 8500,
+        'mangga' : 7800,
+        'duku' : 6500}
+
+print("Daftar buah : apel,jeruk,mangga,duku")
+print(input("Nama buah yang dibeli : "))
+print(input("Berapa Kg             : "))
+print("-----------------------------------")
+print("Total harga           : ")
+
+while pilih:
+    pilih = input("Beli buah yang lain (y/n)? ")
+    if(pilih == "y"):
+        print(input("Nama buah yang dibeli : "))
+        print(input("Berapa Kg             : "))
+    else:
+        break
+
+        while menu:
+            menu = ("A. Tambah data buah, B. Beli buah")
+            if(menu == "A. Tambah data buah"):
+                print(input("Masukkan nama buah    :")
+                print(input("Masukkan harga satuan :")
+            else:
+                break
+
+
+del buah[]
+print(buah)
