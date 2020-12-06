f4a5ab9b3a926b56a50ff83b3e1033f4baa5618c
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-12-06 21:36:14 +0700

    Create Chapter08Project9.py

---
+++ Chapter08/Chapter08Project9/Chapter08Project9.py
@@ -0,0 +1,10 @@
+buah = {'apel' : 5000,
+            'jeruk' : 8500,
+            'mangga' : 7800,
+            'duku' : 6500}
+
+print("Daftar buah : apel,jeruk,mangga,duku")
+print(input("Nama buah yang dibeli : "))
+print(input("Berapa Kg             : "))
+print("-----------------------------------")
+print("Total harga           : ")
