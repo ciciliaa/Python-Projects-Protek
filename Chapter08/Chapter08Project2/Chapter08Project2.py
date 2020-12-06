f44dbbbcee37c62e3142c8194ea75051240d2dae
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-12-06 21:35:53 +0700

    Create Chapter08Project2.py

---
+++ Chapter08/Chapter08Project2/Chapter08Project2.py
@@ -0,0 +1,14 @@
+def dataStar(x):
+    a = sum(x)/len(x)
+    b = max(x)
+    c = min(x)
+
+try :
+    bbul = []
+    bil = int(input("Masukkan bilangan bulat: "))
+    bbul.append(bil)
+    i+= 1
+except ValueError :
+    print("Bukan bilangan bulat")
+bbul.sort(reverse=True)
+print(bbul)
