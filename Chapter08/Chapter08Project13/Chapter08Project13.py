75ea32d44eda535362a279cbe81426facaf47b18
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-12-06 21:35:49 +0700

    Create Chapter08Project13.py

---
+++ Chapter08/Chapter08Project13/Chapter08Project13.py
@@ -0,0 +1,12 @@
+nilaiMhs = [{'nim' : 'A01', 'nama' : 'Amir', 'mid' : 50, 'uas' : 80},
+            {'nim' : 'A02', 'nama' : 'Budi', 'mid' : 40, 'uas' : 90},
+            {'nim' : 'A03', 'nama' : 'Cici', 'mid' : 50, 'uas' : 50},
+            {'nim' : 'A04', 'nama' : 'Dedi', 'mid' : 20, 'uas' : 30},
+            {'nim' : 'A05', 'nama' : 'Fifi', 'mid' : 70, 'uas' : 40}]
+
+for r in nilaiMhs:
+    nilaiMid = r.get("mid")
+    nilaiUas = r.get("uas")
+    nilaiAkhir = (nilaiMid + 2*nilaiUas)/3
+    
+print("Nilai tertinggi :," max(nilaiAkhir))
