a0d1ffe4e78f7be6f213078e5777e7b8321309a7
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-12-06 21:35:59 +0700

    Create Chapter08Project4.py

---
+++ Chapter08/Chapter08Project4/Chapter08Project4.py
@@ -0,0 +1,6 @@
+# bayam kangkung woetel selada///menambahkan menghapus menampilkan
+
+sayur = ["bayam", "kangkung", "wortel", "selada"]
+tambah = print(input("Tambah sayur: "))
+sayur. append(tambah)
+print(sayur)
