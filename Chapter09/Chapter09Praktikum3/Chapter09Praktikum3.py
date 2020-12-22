99000f445a3c8950338d69b596d8b8e2445fcb2f
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-12-22 21:05:04 +0700

    Create Chapter09Praktikum3.py

---
+++ Chapter09/Chapter09Praktikum3/Chapter09Praktikum3.py
@@ -0,0 +1,10 @@
+def bintang(n):
+    for i in range(n):
+        if i <= (7):
+            print("*" * (n-(i-1)) + "*" * i + "*" * (i-1))
+        else:
+            print("*" * (1*x) + "*" * (n-i) + "*" * (n(i+1)))
+
+            x = 1
+        print(("*" * (2*i+1)).center(2*n+1))
+        
