d01fc0e9cf56f9e60813b606600a497a467b5f9f
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2020-12-31 22:12:03 +0700

    Create Chapter10Praktikum7.py

---
+++ Chapter10/Chapter10Praktikum7/Chapter10Praktikum7.py
@@ -0,0 +1,14 @@
+text = "SAYA SUKA PYTHON"
+listTulisan = list(teks)
+
+for i in listTulisan:
+    if(huruf.isalpha()):
+        kodeAscii = ord(listTulisan[x])
+        enkripsi = kodeAscii - n
+        listTulisan[x] = chr(enkripsi)
+    else:
+        continue
+
+akhir = "".join(listTulisan)
+return.akhir
+
